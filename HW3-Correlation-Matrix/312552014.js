055de381f83da0503cc0af5eaad26a9f81b2dd49
unknown <f964966828.cs08@nctu.edu.tw>
2023-09-28 17:08:51 +0800

    complete hw3 - correlation matrix

--- HW3-Correlation-Matrix/312552014.js
+++ HW3-Correlation-Matrix/312552014.js
@@ -36,7 +36,8 @@ function correlation_matrix(data, sex) {
     });
 }
 
-d3.csv("abalone.data").then(data => {
+//d3.csv("abalone.data").then(data => {
+d3.csv("http://vis.lab.djosix.com:2023/data/abalone.data").then(data => {
     const old_columns = data.columns;
     data = data.map(d => {
         for (let i = 0; i  < 9; i++) {
@@ -66,13 +67,16 @@ d3.csv("abalone.data").then(data => {
 
 const margin = {top: 60, right: 60, bottom: 60, left: 60},
     width = 520 - margin.left - margin.right,
-    height = 520 - margin.top - margin.bottom;
+    height = 520 - margin.top - margin.bottom,
+    cellSize = width / 8;
 
 const titles = {
     "M": "Male",
     "F": "Female",
     "I": "Infant"
 }
+
+let svgs = {}
 
 function plotData() {   
     let keys = Object.keys(titles);
@@ -82,31 +86,77 @@ function plotData() {
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        svgs[key] = svg;
 
         plotMatrix(svg, key);
-        plotTicks(svg);
+        plotTicks(svg, key);
         plotColorBar(svg);
     })
 }
 
+function toGrayscale(color) {
+    const matches = color.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+    r = +matches[1];
+    g = +matches[2];
+    b = +matches[3];
+    const gray = Math.round(0.299 * r + 0.587 * g + 0.114 * b);
+    return `rgb(${gray}, ${gray}, ${gray})`;
+}
+
+function updateMatrix() {
+    let keys = Object.keys(svgs);
+    keys.forEach(key => {
+        svgs[key].selectAll(".row").remove();
+        svgs[key].selectAll(".cell").remove();
+        plotMatrix(svgs[key], key);
+    });
+}
+
+let select_i = undefined
+let select_j = undefined
+
 function plotMatrix(svg, key) {
-    let cellSize = width / 8;
     let rows = svg.selectAll(".row")
         .data(matrix[key])
         .enter().append("g")
         .attr("class", "row")
-        .attr("transform", (d, i) => "translate(0," + i * cellSize + ")");
+        .attr("transform", (d, i) => "translate(0," + i * cellSize + ")")
 
     let cells = rows.selectAll(".cell")
         .data(d => d)
         .enter().append("g") 
         .attr("class", "cell")
-        .attr("transform", (d, i) => "translate(" + i * cellSize + ",0)");
+        .attr("transform", (d, i) => "translate(" + i * cellSize + ",0)")
 
-    cells.append("rect")
-        .attr("width", cellSize)
-        .attr("height", cellSize)
-        .style("fill", d => d3.interpolateRdBu(0.5 - d/2));
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            d3.select(cells._groups[i][j])
+                .on("mouseover", function(d) {
+                    if (select_i != i || select_j != j) {
+                        select_i = i;
+                        select_j = j;
+                        updateMatrix();
+                    }
+                })
+                .on("mouseout", function(event, d) {
+                    if ((i == 0 || i == 7) || (j == 0 || j == 7)) {
+                        select_i = undefined;
+                        select_j = undefined;
+                        updateMatrix();
+                    }
+                }).append("rect")
+                    .attr("width", cellSize)
+                    .attr("height", cellSize)
+                    .style("fill", function(d) {
+                        let color = d3.interpolateRdBu(0.5 - d/2);
+                        if (select_i === undefined || (select_i == i && select_j == j)) {
+                            return color;
+                        } else {
+                            return toGrayscale(color);
+                        }
+                    });
+        }
+    }
 
     cells.append("text")
         .attr("x", cellSize / 2) 
@@ -115,18 +165,9 @@ function plotMatrix(svg, key) {
         .attr("text-anchor", "middle")  
         .attr("fill", d => (Math.abs(d) > 0.5 ? "white" : "black"))
         .text(d => d.toFixed(2));
-
-    // Add title
-    svg.append("text")
-        .attr("class", "title")
-        .attr("x", width / 2)
-        .attr("y", -20) // Adjust vertical position of the title
-        .style("text-anchor", "middle")
-        .style("font-size", "40px")
-        .text(titles[key]);
 }
 
-function plotTicks(svg) {
+function plotTicks(svg, key) {
     // Add x-axis ticks for columns
     let xScale = d3.scaleBand().domain(d3.range(0, 8)).range([0, width]);
     let xAxis = d3.axisTop(xScale).tickSize(0).tickFormat(i => `Attr ${i + 1}`);
@@ -152,6 +193,15 @@ function plotTicks(svg) {
         .style("text-anchor", "end")
         .style("font-size", "16px")
         .attr("dx", "-0.5em"); // Adjust horizontal distance of tick labels from axis line
+
+    // Add title
+    svg.append("text")
+        .attr("class", "title")
+        .attr("x", width / 2)
+        .attr("y", -20) // Adjust vertical position of the title
+        .style("text-anchor", "middle")
+        .style("font-size", "40px")
+        .text(titles[key]);
 }
 
 function plotColorBar(svg) {
